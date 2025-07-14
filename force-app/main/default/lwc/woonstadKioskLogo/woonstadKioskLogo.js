3b8a460900041a870af11b9b2883a0ff8c5b9e3a
dennismusschenbroek-kristoffers <default@cloudity.com>
2025-07-14 15:33:09 +0200

    🔁 DevA metadata snapshot - Mon Jul 14 15:33:09 CEST 2025

--- force-app/main/default/lwc/woonstadKioskLogo/woonstadKioskLogo.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import LOGO_URL from '@salesforce/resourceUrl/WSRLogo';
-
-export default class WoonstadKioskLogo extends LightningElement {
-    logoUrl = LOGO_URL;
-}
